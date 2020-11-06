b9db6aca339ad29fb536305d2466a6df4bf9491a
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-11-06 21:40:35 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan31-PerkenalanMahasiswa/src/pboif2/pkg10119083/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * Nama : Rafsan Zen Mustaofa 
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Program Perkenalan Mahasiswa
+ */
+public class Mahasiswa {
+    public String nama;
+    public String nim;
+    
+    
+     public void perkenalkanDiri() {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM " + nim);
+        System.out.println("My Name is " + nama);
+        System.out.println();
+}
+}
---
+++ PBOIF2-10119083-Latihan31-PerkenalanMahasiswa/src/pboif2/pkg10119083/latihan31/perkenalanmahasiswa/PBOIF210119083Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * Nama : Rafsan Zen Mustaofa 
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Menamplkan Perkenalan Mahasiswa
+ */
+public class PBOIF210119083Latihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Mahasiswa mhs = new Mahasiswa();
+         
+        mhs.nim = "10119081";
+        mhs.nama = "Muammad Elza Abiezal";
+
+        mhs.perkenalkanDiri(); 
+        
+        mhs.nim = "10119082";
+        mhs.nama = "Zainull Rifqi";
+
+        mhs.perkenalkanDiri();
+
+        mhs.nim = "10119083";
+        mhs.nama = "Rafsan Zen Mustaofa";
+
+        mhs.perkenalkanDiri();
+
+        mhs.nim = "10119084";
+        mhs.nama = "Muammad Idris Merdefi";
+
+        mhs.perkenalkanDiri();
+
+       
+        
+        System.out.println("-------------------------------------");
+        System.out.println("Developed by : Rafsan Zen Mustaofa");
+    }}
+    
+
